b18a08fe6f33d92ed87736c75038ddb519143733
Semyen Gabyshev <semyen.gabyshev@noveogroup.com>
2015-07-09 16:41:33 +0600

    validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ActiveRecord::Base
   belongs_to :gallery
+
+  validates :size, presence: true, numericality: { only_integer: true }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   has_many :galleries, dependent: :destroy
   accepts_nested_attributes_for :galleries, reject_if: :all_blank, allow_destroy: true
+  validates_presence_of :name
 end
