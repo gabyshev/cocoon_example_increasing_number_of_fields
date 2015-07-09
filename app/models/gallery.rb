fa08929a331f7826d4becadde298288072371415
Semyen Gabyshev <semyen.gabyshev@noveogroup.com>
2015-07-09 16:52:05 +0600

    create photoms after initialize gallery

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,5 +1,14 @@
 class Gallery < ActiveRecord::Base
   belongs_to :user
   has_many :photos, dependent: :destroy
+
   accepts_nested_attributes_for :photos, reject_if: :all_blank, allow_destroy: true
+
+  after_initialize :make_photos
+
+  private
+
+  def make_photos
+    6.times { photos.build }
+  end
 end
